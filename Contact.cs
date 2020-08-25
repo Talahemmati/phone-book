a4fd5416be65537bb5edeaef70bc9de3da89a4b1
Talahemmati <69954495+Talahemmati@users.noreply.github.com>
2020-08-25 12:30:42 +0430

    Add files via upload

---
+++ Contact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Contact
+    {
+        public string firstname { get; set; }
+        public string familyname { get; set; }
+        public string phonenumber { get; set; }
+
+
+        public Contact()
+        {
+            this.firstname = null;
+            this.firstname = null;
+            this.phonenumber = null;
+        }
+        public Contact(string firstname, string familyname, string phonenumber)
+        {
+            this.firstname = firstname;
+            this.firstname = familyname;
+            this.phonenumber = phonenumber;
+        }
+        public override string ToString()
+        {
+            return "First name: " + this.firstname + " \n" +
+                   "Family name: " + this.familyname + " \n" +
+                   "Phonenumber: " + this.phonenumber+" \n";
+        }
+    }
+
+}
+    
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("****************************");
+            Console.ResetColor();
+            Contact Cont = new Contact();
+            Cont.firstname = "Tala";
+            Cont.phonenumber = ("09058172622");
+            Console.WriteLine(Cont);
+            Console.ReadKey();
+
+  
+        }
+    }
+}
